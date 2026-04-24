95d5bdc474cbaae43d0e237e72b1ec4edec49a59
BLABLU-ALT <jezielduarte.jd@gmail.com>
2026-04-24 19:56:10 -0300

    cotacao 3

--- app/Http/Controllers/CotacaoController.php
+++ app/Http/Controllers/CotacaoController.php
@@ -12,14 +12,17 @@ class CotacaoController extends Controller
         try {
             $resposta = Http::timeout(5)
                 ->get("https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL");
+
             if ($resposta->successful()) {
                 $dados = $resposta->json();
+
                 return view('cotacoes.index', ['cotacoes' => $dados]);
             }
-            return back()->with('erro', 'Falha ao conectar com a API.');
+
+            return redirect()->back()->with('erro', 'Falha ao conectar com a API.');
 
         } catch (ConnectionException $e) {
-            return back()->with('erro', 'A API demorou para responder.');
+            return redirect()->back()->with('erro', 'A API demorou para responder.');
         }
     }
 }
